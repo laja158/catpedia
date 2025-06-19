e41c85c034b84ac35ecb119c42f1973cc440f013
jose.acosta <jose.acosta@sofka.com.co>
2025-06-19 09:02:32 -0500

    feat: update production environment configuration with API keys and URLs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: 'live_99Qe4Ppj34NdplyLW67xCV7Ds0oSLKGgcWWYnSzMJY9C0QOu0HUR4azYxWkyW2nr',
+  apiUrlBreeds: 'https://api.thecatapi.com/v1/breeds',
+  apiUrlImages: 'https://api.thecatapi.com/v1/images/search?breed_ids='
 };
